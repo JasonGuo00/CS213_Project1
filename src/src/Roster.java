50f1f753c312ae99671b3d0e3f9b45ea01dcb29f
JasonGuo00 <jasonglobguo@gmail.com>
2023-01-30 18:48:47 -0500

    Roster Class Methods
    Outline for what methods need to be filled out in the Roster Class

--- src/src/Roster.java
+++ src/src/Roster.java
@@ -1,4 +1,31 @@
 package src;
 
 public class Roster {
+    private Student[] roster;
+    private int size;
+
+    private int find() {
+        return 0;
+    }
+    private void grow() {
+
+    }
+    public boolean add(Student student) {
+        return false;
+    }
+    public boolean remove(Student student) {
+        return false;
+    }
+    public boolean contains(Student student) {
+        return false;
+    }
+    public void print() {
+
+    }
+    public void printBySchoolMajor() {
+
+    }
+    public void printByStanding() {
+
+    }
 }
