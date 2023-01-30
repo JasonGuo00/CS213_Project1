26f9b7be29c8b998b9a59542a2aae8b14fa92e32
JasonGuo00 <jasonglobguo@gmail.com>
2023-01-30 11:55:23 -0500

    Enums established
    Filled in the Enum class with the majors and their associated course codes and schools

--- src/src/Major.java
+++ src/src/Major.java
@@ -1,5 +1,16 @@
 package src;
 
 public enum Major {
+    CS ("01:198", "SAS"),
+    MATH ("01:640", "SAS"),
+    EE ("14:332", "SOE"),
+    ITI ("04:547", "SC&I"),
+    BAIT ("33:136", "RBS");
 
+    private final String code;
+    private final String school;
+    Major(String code, String school) {
+        this.code = code;
+        this.school = school;
+    }
 }
