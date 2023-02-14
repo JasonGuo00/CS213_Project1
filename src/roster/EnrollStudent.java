29042226135792e0e57e63a7195bae022d0020bb
JasonGuo00 <jasonglobguo@gmail.com>
2023-02-13 19:35:55 -0500

    Enroll and EnrollStudent Classes
    - Enroll and EnrollStudent should be implemented properly
    - Add and Remove in Enroll do not return anything
    - There is no sorting necessary in Enroll

--- src/roster/EnrollStudent.java
+++ src/roster/EnrollStudent.java
@@ -4,13 +4,22 @@ public class EnrollStudent  {
     private Profile profile;
     private int creditsEnrolled;
 
+    public EnrollStudent(String lname, String fname, Date dob, int creditsEnrolled) {
+        profile = new Profile(lname, fname, dob);
+        this.creditsEnrolled = creditsEnrolled;
+    }
+
     @Override
     public String toString() {
-        return "A";
+        return profile.toString() + " enrolled in " + creditsEnrolled + " credits";
     }
 
     @Override
     public boolean equals(Object obj) {
+        if(obj instanceof EnrollStudent) {
+            EnrollStudent student = (EnrollStudent)obj;
+            return profile.equals(student.profile) && creditsEnrolled == student.creditsEnrolled;
+        }
         return false;
     }
 }
--- src/roster/Enrollment.java
+++ src/roster/Enrollment.java
@@ -4,14 +4,49 @@ public class Enrollment {
     private EnrollStudent[] enrollStudents;
     private int size;
 
-    public void add(EnrollStudent enrollStudent) {
+    public Enrollment() {
+        enrollStudents = new EnrollStudent[Constants.ROSTER_CAPACITY];
+        size = 0;
+    }
 
+    private int find(EnrollStudent student) {
+        for (int i = 0; i < size; i++) {
+            if (student.equals(enrollStudents[i])) {
+                return i;
+            }
+        }
+        return Constants.NOT_FOUND;
+    }
+
+    private void grow() {
+        EnrollStudent[] new_enrollStudents = new EnrollStudent[enrollStudents.length + Constants.ROSTER_INC];
+        for (int i = 0; i < size; i++) {
+            new_enrollStudents[i] = enrollStudents[i];
+        }
+        enrollStudents = new_enrollStudents;
+    }
+
+    public void add(EnrollStudent enrollStudent) {
+        if(size+1 == enrollStudents.length) {
+            this.grow();
+        }
+        enrollStudents[size] = enrollStudent;
+        size++;
     }
     public void remove(EnrollStudent enrollStudent) {
-
+        int index = find(enrollStudent);
+        if(index != Constants.NOT_FOUND && index != size - 1) {
+            enrollStudents[index] = enrollStudents[size-1];
+            enrollStudents[size-1] = null;
+            size--;
+        }
+        else if(index == size-1) {
+            enrollStudents[index] = null;
+            size--;
+        }
     }
     public boolean contains(EnrollStudent enrollStudent) {
-        return false;
+        return find(enrollStudent) != Constants.NOT_FOUND;
     }
     public void print() {
         for(int i = 0; i < size; i++) {
